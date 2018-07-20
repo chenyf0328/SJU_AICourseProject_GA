ad0b340386e7b768db976543a028e18f8e4a6889
chenyf0328 <826787530@qq.com>
2018-07-20 17:15:23 +0800

    Add files via upload

---
+++ PlotPackage/Src/PlotTester.java
@@ -0,0 +1,17 @@
+
+import jahuwaldt.plot.PlotWindow;
+
+/**
+*  A simple class for testing my plotting package.
+*  See the source code for jahuwaldt.plot.PlotWindow for
+*  examples of how to set up and modify plots.
+**/
+public class PlotTester {
+
+    public static void main(String args[]) {
+	
+		//	Simply call the testing code contained in the PlotWindow class.
+        PlotWindow.main(null);
+    }
+
+}
